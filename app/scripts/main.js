85f3cefe7728ce550011b1bbe7132c80aef7ab48
salehkaddoura <saleh.kaddoura@gmail.com>
2015-04-07 23:55:03 -0700

    started website redesign

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,4 +2,14 @@ $(document).ready(function() {
   // ACTIVATE MATERIALIZE COMPONENTS
   $('.dropdown-button').dropdown();
   $('.button-collapse').sideNav();
+
+  $('.hhs-slider').slick({
+    autoplay: true,
+    arrows: true,
+    dots: true,
+    draggable: true,
+    infinite: true,
+    slidesToShow: 1
+    // adaptiveHeight: true
+  })
 });

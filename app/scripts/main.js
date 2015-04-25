1bff4ab79ed50d9f206e435ce89589119251951b
salehkaddoura <saleh.kaddoura@gmail.com>
2015-04-25 02:22:37 -0700

    created all services pages, added content to services pages

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,13 +3,13 @@ $(document).ready(function() {
   $('.dropdown-button').dropdown();
   $('.button-collapse').sideNav();
 
-  $('.hhs-slider').slick({
-    autoplay: true,
-    arrows: true,
-    dots: true,
-    draggable: true,
-    infinite: true,
-    slidesToShow: 1
-    // adaptiveHeight: true
-  })
+  // $('.hhs-slider').slick({
+  //   autoplay: true,
+  //   arrows: true,
+  //   dots: true,
+  //   draggable: true,
+  //   infinite: true,
+  //   slidesToShow: 1
+  //   // adaptiveHeight: true
+  // })
 });

cb5c481292b8b36e08eb0c61913c19f4c14e9558
salehkaddoura <saleh.kaddoura@gmail.com>
2015-04-25 03:06:23 -0700

    added contact form support

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var errorHandler = require('errorhandler');
+var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+
 var port = process.env.PORT || 8080;
 process.env.NODE_ENV = 'development';
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.set('views', __dirname + '/app');
 app.set('view engine', 'html');
 app.engine('html', require('hbs').__express);
@@ -30,6 +35,32 @@ app.get('/contact', function(req, res) {
 app.get('/careers', function(req, res) {
     res.render('careers');
 });
+
+app.post('/contacthhs', function(req, res) {
+    console.log(req.body);
+    var transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+            user: 'saleh.kaddoura@gmail.com',
+            pass: 'smileyrilec93'
+        }
+    });
+
+    var mailOptions = {
+        from: req.body.fullname + ' ' + req.body.email,
+        to: 'saleh.kaddoura@gmail.com',
+        subject: 'New Customer',
+        text: req.body.message
+    }
+
+    transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Message sent: ', info.response);
+        }
+    });
+})
 
 app.listen(port);
 console.log('The magic happens on port ' + port);
